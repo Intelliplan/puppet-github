8e32cd510e98405d6796e48416bc4a59e5e7a806
neochrome <johan@stenqvist.net>
2013-10-17 08:23:49 +0200

    github: added module to download and extract tarballs from github

---
+++ manifests/init.pp
@@ -0,0 +1,22 @@
+class github {
+  package { ['curl','tar']: }
+}
+
+define github::tarball (
+  $extract_to = undef,
+  $repo       = $title,
+  $ref        = 'master',
+) {
+  require github
+
+  if $extract_to == undef { fail('extract_to is required') }
+  if $repo == undef { fail('repo is required') }
+  if $ref == undef { fail('ref is required') }
+  
+  $github_url = "https://github.com/${repo}/archive/${ref}.tar.gz"
+  exec { "${title}::download_and_extract":
+    command => "curl -sSL \"${github_url}\" | tar -C \"${extract_to}\" --strip-components=1 --no-same-owner -xzf -",
+    path    => ['/usr/local/bin', '/bin', '/usr/bin'],
+    require => Exec["${title}::ensure::path"],
+  }
+}
